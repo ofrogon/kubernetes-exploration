48c1d1714a71aca72c5eb1aff0e51f310d08a604
fvgoulet <flx890@gmail.com>
2019-08-16 01:46:42 -0400

    Added subscription selector for Terraform in setup.ps1

--- Terraform/DemoVault/Terraform/setup.ps1
+++ Terraform/DemoVault/Terraform/setup.ps1
@@ -1,17 +1,18 @@
-# Warning:
-# This script considers that you have only one subscription and / or that the good subscription is currently used
-
 # Get Subscription and tenant ID
-$accountDetails = az account show --query "{subscriptionId:id, tenantId:tenantId}" | ConvertFrom-Json
+# id => subscriptionId
+$accountDetails = az account list | ConvertFrom-Json
+if($accountDetails.Count -and $accountDetails.Count -gt 1) {
+  $accountDetails = $accountDetails | Out-GridView -OutputMode Single
+}
 
 # Create a ServicePrincipal for Terraform and will need appId and password
-$terraformUser = az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/$($accountDetails.subscriptionId)" | ConvertFrom-Json
+$terraformUser = az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/$($accountDetails.id)" | ConvertFrom-Json
 
 # Set the environment variables for Terraform
-[Environment]::SetEnvironmentVariable("ARM_SUBSCRIPTION_ID", $accountDetails.subscriptionId, "User")
-[Environment]::SetEnvironmentVariable("ARM_CLIENT_ID",       $terraformUser.appId,           "User")
-[Environment]::SetEnvironmentVariable("ARM_CLIENT_SECRET",   $terraformUser.password,        "User")
-[Environment]::SetEnvironmentVariable("ARM_TENANT_ID",       $accountDetails.tenantId,       "User")
+[Environment]::SetEnvironmentVariable("ARM_SUBSCRIPTION_ID", $accountDetails.id,       "User")
+[Environment]::SetEnvironmentVariable("ARM_CLIENT_ID",       $terraformUser.appId,     "User")
+[Environment]::SetEnvironmentVariable("ARM_CLIENT_SECRET",   $terraformUser.password,  "User")
+[Environment]::SetEnvironmentVariable("ARM_TENANT_ID",       $accountDetails.tenantId, "User")
 
 # Activate the Azure API preview features (for Windows node creation)
 # https://github.com/Azure/AKS/blob/master/previews.md
